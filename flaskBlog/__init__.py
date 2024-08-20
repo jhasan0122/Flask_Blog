ffccc6924081f4ce79c57aaf2b68d6ce2b69d980
Jehan Hasan <jhasan0122@gmail.com>
2024-08-20 17:08:31 +0600

    update-delete

--- flaskBlog/__init__.py
+++ flaskBlog/__init__.py
@@ -4,12 +4,14 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '066bb57df532020449df021398de2628'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+app = Flask(__name__)                               # For flask __main__ #
+app.config['SECRET_KEY'] = '066bb57df532020449df021398de2628'   # For generate a sample secret key and save it #
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'    # For database uri  #
+db = SQLAlchemy(app)                                          # database initialization  #
+bcrypt = Bcrypt(app)                                           # encryption initialization #
+login_manager = LoginManager(app)                              # create a login manager object for authorization
+login_manager.login_view = 'login'                            # for showing Please log in to access this page. for direct account access #
+login_manager.login_message_category = 'info'                # bootstrap class add #
 
-from flaskBlog import routes
+
+from flaskBlog import routes                              # import rest of the route element #
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from flaskBlog import app
 
-if __name__ == "__main__":
+if __name__ == "__main__":    # For run the project #
     app.run(debug=True)
